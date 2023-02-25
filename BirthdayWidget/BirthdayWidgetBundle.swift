db157d415907211c559f789c02009511cd762a71
Efremov Oleg <o.v.efremov@me.com>
2023-02-25 16:31:12 +0300

    Тестовый виджет

---
+++ BirthdayWidget/BirthdayWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BirthdayWidgetBundle.swift
+//  BirthdayWidget
+//
+//  Created by Ефремов Олег on 25.02.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BirthdayWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BirthdayWidget()
+    }
+}
