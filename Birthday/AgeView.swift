f465e6fd802faa9a6fd9e20841b8e368e846bcb1
Ефремов Олег <o.v.efremov@me.com>
2023-03-22 21:17:05 +0300

    Локализация AgeView

--- Birthday/AgeView.swift
+++ Birthday/AgeView.swift
@@ -1,32 +1,34 @@
 import SwiftUI
 import Contacts
 
+
 struct AgeView: View {
     let contact: CNContact
     let today: Date
-
-    var age: Int! {
-        guard let cbYear  = contact.birthday!.year else { return nil }
-        guard let cbMonth = contact.birthday!.month else { return nil }
-        guard let cbDay   = contact.birthday!.day else { return nil }
-        
-        let age = today.year - cbYear
-
-        let tMonth = today.month
-        let tDay = today.day
-        
-        let delta = tMonth < cbMonth ? 0 : (tMonth > cbMonth ? 1 : (tDay <= cbDay ? 0 : 1) )
-        return age + delta
+    
+    var age: Int? {
+        guard let b = contact.birthday, let cbYear = b.year, let cbMonth = b.month, let cbDay = b.day else { return nil }
+        let delta = today.month < cbMonth ? 0 : ( today.month > cbMonth ? 1 : (today.day <= cbDay ? 0 : 1) )
+        return (today.year - cbYear) + delta
     }
     
+    private func ageString(count: Int?) -> String {
+        guard let count else { return "" }
+        let formatString = NSLocalizedString("ages.count", comment: "")
+        let resultString = String(format: formatString, count)
+        return resultString
+    }
+    
+    
     var body: some View {
-        Text(age != nil ? "\(age!) года" : "")
-        Text("\(age) года")
+        Text(ageString(count: age))
     }
 }
+
 
 struct AgeView_Previews: PreviewProvider {
     static var previews: some View {
         AgeView(contact: dataSet1[0], today: Date())
+            .environment(\.locale, .init(identifier: "ru"))
     }
 }
